1eb5e6250050049d5100250f5b252e1017d15930
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-01-30 23:27:10 +0200

    Add class Pegs, refactor code
    +class Pegs(UserDict):

--- move_disk_mod.py
+++ move_disk_mod.py
@@ -1,47 +1,45 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+from collections import UserDict
 
 
-Peg = list[int]
-NDISKS = 3
-from_: Peg = [*range(NDISKS, 0, -1)]
-to: Peg = []
-aux: Peg = []
+class Pegs(UserDict):
+    def __init__(self, ndisks):
+        data = {
+            "A": list(range(ndisks, 0, -1)),
+            "B": [],
+            "C": [],
+        }
+        super().__init__(data)
 
-PEGS = {
-    "A": [3, 2, 1],
-    "B": [],
-    "C": [],
-}
-
-
-def move(from_, to, pegs=PEGS):
-    src, dst = pegs[from_], pegs[to]
-    if dst and src[-1] > dst[-1]:
-        raise ValueError(f"Cannot place {src[-1]} on top {dst[-1]}")
-    disk = src.pop()
-    dst.append(disk)
+    def move(self, from_, to):
+        src, dst = self.data[from_], self.data[to]
+        if dst and src[-1] > dst[-1]:
+            raise ValueError(f"Cannot place {src[-1]} on top {dst[-1]}")
+        disk = src.pop()
+        dst.append(disk)
 
 
-def solve3():
+def solve3(pegs: Pegs) -> Pegs:
     """
-    >>> solve3()
-    >>> PEGS["A"]
+    >>> pegs = Pegs(3)
+    >>> solve3(pegs)
+    >>> pegs["A"]
     []
-    >>> PEGS["C"]
+    >>> pegs["C"]
     [3, 2, 1]
-    >>> PEGS["B"]
+    >>> pegs["B"]
     []
     >>>
     """
-    move("A", "C")
-    move("A", "B")
-    move("C", "B")
-    move("A", "C")
-    move("B", "A")
-    move("B", "C")
-    move("A", "C")
+    pegs.move("A", "C")
+    pegs.move("A", "B")
+    pegs.move("C", "B")
+    pegs.move("A", "C")
+    pegs.move("B", "A")
+    pegs.move("B", "C")
+    pegs.move("A", "C")
 
 
 if __name__ == "__main__":
